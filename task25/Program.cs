50acc18bd84e28b48bade5263279f8dc0fd507ce
Nickolay <katin@mephi.rugit config --global user.email katin@mephi.ru>
2023-08-08 13:55:54 +0300

    comment

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Write("Введите основание: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int Sum(int a, int b)
+{
+    int pow = 1;
+    for (int i=1; i <= b; i++)
+    {
+        pow = pow * a;
+    }
+    return pow;
+}
+int result = Sum(x, y);
+Console.WriteLine(result);
---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+ int Sum()
+ {
+    int s = 0;
+    while(a > 0)
+    {
+        s += a%10;
+        a /= 10;
+    }
+    return s;
+ }
+ int result = Sum();
+ Console.WriteLine(result);
+ 
